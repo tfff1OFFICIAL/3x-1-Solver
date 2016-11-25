08a8c2bed57aeafa3712997aba96858cb33122e7
Tfff1OFFICIAL <tfinlay2011@gmail.com>
2016-11-25 20:18:15 +1300

    Fixed Iteration error

--- calculate.py
+++ calculate.py
@@ -1,6 +1,7 @@
-from __future__ import division
+#from __future__ import division
+import math
 
-
+# Replaced - Caused maximum recursion limit breaches for certain numbers.
 def get_final_number(n, count = -1):
     count += 1
     final_count = count
@@ -14,6 +15,17 @@ def get_final_number(n, count = -1):
             n, final_count = get_final_number((3 * n) + 1, count)
     return n, final_count
 
+def get_number(n):
+    recursion_count = -1
+    while n != 1:
+        recursion_count += 1
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = (3 * n) + 1
+        print("Iteration %s returns: %s" % (recursion_count, n))
+    return n, recursion_count
+
 while True:
     number = input("Enter a starting number: ")
-    print("Returned %s on iteration %s." % get_final_number(int(number)))
+    print("Returned %s on iteration %s." % get_number(int(number)))
